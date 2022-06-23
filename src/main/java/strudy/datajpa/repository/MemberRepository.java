ab2741844f8bc70482a4925d02b89f3f09096533
seunghwanLee <dkqlch@naver.com>
2022-06-23 23:41:50 +0900

    사용자 정의 리포지토리 구현

--- src/main/java/strudy/datajpa/repository/MemberRepository.java
+++ src/main/java/strudy/datajpa/repository/MemberRepository.java
@@ -62,5 +62,6 @@ public interface MemberRepository extends JpaRepository<Member, Long>,MemberRepo
     Member findReadOnlyByUsername(String username);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<Member> findLockByUsername(String username);
 
 }
